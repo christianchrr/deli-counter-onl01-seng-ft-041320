cdc0322d20c6a84228277eaea4106f6865971b76
Christian Cherry <christianchrr@gmail.com>
2020-04-17 09:04:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,8 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{i + 1}. #{katz_deli.join(" ")}"
+    num = 1
+    katz_deli.each do |customer|
   end
 end
 
