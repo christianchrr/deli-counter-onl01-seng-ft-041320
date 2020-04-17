a627aa48e9a36760924ca6a4c6c701d941efa218
Christian Cherry <christianchrr@gmail.com>
2020-04-17 08:36:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katyz_deli = []
+
+def line(katz_deli)
+  line_position = []
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else 
+    number = 1
+    katz_deli.each do |customer|
+      line_position << "#{number}. #{customer}"
+      number += 1
+    end
+    puts "The line is currently #{line_postion.join(" "}"
+  end
+end
