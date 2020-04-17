b15cd7c095f7a146f14c9d18c2dd2f7a10c520e0
Christian Cherry <christianchrr@gmail.com>
2020-04-17 08:50:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else 
-    puts katz_deli
+    puts "There are currently #{katz_deli.length} people in line."
   end
 end
 
